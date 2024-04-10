5757d40d6761394409f94499be4088e58791063b
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-10 15:21:06 -0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+importa psutil
+import datetime as dt
+
+def log_ram_usage():
+    with open('log_ram.txt', 'a+') as f:
+        f.write(f'RAM USAGE{dt.datetime.now()}: {psutil.virtual_memory().percent}%\n')
+
+
+def log_cpu_usage():
+    with open('log_cpu.txt', 'a+') as f:
+        f.write(f'CPU USAGE{dt.datetime.now()}: {psutil.cpu_percent()}%\n')
+
+
+def print_message(message):
+    print('Message:' message)
